bffa53d5a7aab41b4b9e367335b42fd6f9e41df9
egomezs <edisonjaviergomezs@gmail.com>
2024-06-23 16:26:12 -0500

    dockerfile webapp.war

--- server/test/java/com/example/TestGreeter.java
+++ server/test/java/com/example/TestGreeter.java
@@ -3,6 +3,8 @@ package com.example;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.example.service.Greeter;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
